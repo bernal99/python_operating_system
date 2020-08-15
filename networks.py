22f16af08186054c3fcb16e9d1f17420b2535983
jesus-bernal <jjbm990118@hotmail.com>
2020-08-15 12:21:08 -0500

    Actually just saving it

--- networks.py
+++ networks.py
@@ -4,13 +4,11 @@ import socket
 
 def check_localhost():
         localhost = socket.gethostbyname('localhost')
-        print(localhost)
         if localhost == '127.0.0.1':
                 return True
 
 def check_connectivity():
         request = requests.get("http://www.google.com")
-        print(request)
         if request.status_code == 200:
                 return True
 
